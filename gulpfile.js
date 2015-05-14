1c327149a65bbda435263d1e3588be939cfb6b17
Jason Raimondi <jason@raimondi.us>
2015-05-14 16:34:51 -0400

    include normalize-scss in bower and in compiled sass

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,17 @@ var gulp = require('gulp'),
 
 gulp.task('styles', function() {
   return gulp.src(paths.SOURCE + 'sassyflex.scss')
-		.pipe(sourcemaps.init())
+	.pipe(sourcemaps.init())
     .pipe(sass({
-    		style: 'expanded',
-				includePaths : [
-				       paths.BOWER + 'bourbon/app/assets/stylesheets',
-				       paths.BOWER + 'neat/app/assets/stylesheets',
-				   ]
-    	}).on('error', sass.logError))
+		style: 'expanded',
+			includePaths : [
+                   paths.BOWER + 'normalize-scss',
+			       paths.BOWER + 'bourbon/app/assets/stylesheets',
+			       paths.BOWER + 'neat/app/assets/stylesheets',
+			   ]
+	}).on('error', sass.logError))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-		.pipe(sourcemaps.write('./maps'))
+	.pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest(paths.DESTINATION))
     .pipe(notify({ message: 'Styles task complete' }));
 });
